a34416f2184128b9d737e7744943dd611524bffa
NeilK-94 <neil.kyne@gmail.com>
2020-03-16 19:15:37 +0000

    Asteroids spawn in waves at random times

---
+++ Assets/__Scripts/DestroyByTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+    //  Life time of game object attached to
+    public float lifeTime;
+
+    void Start()
+    {   //  Destroy game object attached to after lifetime is up
+        Destroy(gameObject, lifeTime);
+    }
+}
--- Assets/__Scripts/GameController.cs
+++ Assets/__Scripts/GameController.cs
@@ -6,17 +6,30 @@ public class GameController : MonoBehaviour
 {
     //  Asteroids
     public GameObject hazard;
+    public int hazardCount;
     public Vector3 spawnValues;
 
+    public float spawnDelay;
+    public float startDelay;
+    public float waveDelay;
     private void Start()
     {
-        SpawnWaves();
+       StartCoroutine(SpawnWaves());
     }
-    void SpawnWaves()
+    IEnumerator SpawnWaves()
     {
-        //  Set spawn position of asteroids to be a random range between specified values in the editor (length of screen)
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-        Quaternion spawnRotation = Quaternion.identity;
-        Instantiate(hazard, spawnPosition, spawnRotation);
+        yield return new WaitForSeconds(startDelay);
+        while (true) {
+            for (int i = 0; i < hazardCount; i++) {
+                //  Set spawn position of asteroids to be a random range between specified values in the editor (length of screen)
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+
+                yield return new WaitForSeconds(spawnDelay);
+            }
+            yield return new WaitForSeconds(waveDelay);
+
+        }
     }
 }
