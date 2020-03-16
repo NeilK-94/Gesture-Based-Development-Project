a891d3c580c58c562a979080fd9b502d74df07ef
NeilK-94 <neil.kyne@gmail.com>
2020-03-16 18:26:28 +0000

    Added asteroids for player to dodge

---
+++ Assets/__Scripts/DestroyByContact.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    //  When something enters this collider, destroy it 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Boundary")
+        {
+            return;
+        }
+        Destroy(other.gameObject);  //  Destroy bullet
+        Destroy(gameObject);    //  Destroy asteroid
+    }
+}
---
+++ Assets/__Scripts/RandomRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        //  Gves a random V3 value and set to rb's angular velocity
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
