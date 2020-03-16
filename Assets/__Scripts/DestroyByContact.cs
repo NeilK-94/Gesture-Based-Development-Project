7f6db8751e20f59e2d465c215c7396eb988b9c50
NeilK-94 <neil.kyne@gmail.com>
2020-03-16 18:46:11 +0000

    Game controller now managing random spawning of asteroids

--- Assets/__Scripts/DestroyByContact.cs
+++ Assets/__Scripts/DestroyByContact.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject asteroidExplosion;
+    public GameObject playerExplosion;
+
     //  When something enters this collider, destroy it 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Boundary")
         {
             return;
+        }
+        //  Instantiate explosions
+        Instantiate(asteroidExplosion, transform.position, transform.rotation);
+        if(other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+
         }
         Destroy(other.gameObject);  //  Destroy bullet
         Destroy(gameObject);    //  Destroy asteroid
---
+++ Assets/__Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //  Asteroids
+    public GameObject hazard;
+    public Vector3 spawnValues;
+
+    private void Start()
+    {
+        SpawnWaves();
+    }
+    void SpawnWaves()
+    {
+        //  Set spawn position of asteroids to be a random range between specified values in the editor (length of screen)
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity;
+        Instantiate(hazard, spawnPosition, spawnRotation);
+    }
+}
